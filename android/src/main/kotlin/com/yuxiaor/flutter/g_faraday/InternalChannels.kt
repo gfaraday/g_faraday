93e61260135c1e4101750a836a36502513303710
Edward <edward6934@dingtalk.com>
2020-09-29 10:19:49 +0800

    add:InternalChannel

---
+++ android/src/main/kotlin/com/yuxiaor/flutter/g_faraday/InternalChannels.kt
@@ -0,0 +1,27 @@
+package com.yuxiaor.flutter.g_faraday
+
+import io.flutter.plugin.common.MethodChannel
+
+/**
+ * Author: Edward
+ * Date: 2020-09-28
+ * Description:桥接通道
+ */
+internal object InternalChannels {
+    //网络
+    private val netChannel by lazy { MethodChannel(Faraday.engine.dartExecutor, "g_faraday/net") }
+
+    //通用
+    private val commonChannel by lazy { MethodChannel(Faraday.engine.dartExecutor, "g_faraday/common") }
+
+
+    fun setNetHandler(handler: MethodChannel.MethodCallHandler) {
+        netChannel.setMethodCallHandler(handler)
+    }
+
+
+    fun setCommonHandler(handler: MethodChannel.MethodCallHandler) {
+        commonChannel.setMethodCallHandler(handler)
+    }
+
+}
