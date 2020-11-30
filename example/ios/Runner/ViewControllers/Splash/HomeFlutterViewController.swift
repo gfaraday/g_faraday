106ce95980c2bb8dfb7a219a2d6aa4cbe8debe61
gix <aoxianglele@icloud.com>
2020-11-30 15:15:05 +0800

    feat: add example: ios load default splash screen

--- example/ios/Runner/ViewControllers/Splash/HomeFlutterViewController.swift
+++ example/ios/Runner/ViewControllers/Splash/HomeFlutterViewController.swift
@@ -14,18 +14,20 @@ class HomeFlutterViewController: FaradayFlutterViewController {
         super.init("home", arguments: nil, backgroundClear: false, engine: nil, callback: nil)
         navigationController?.setNavigationBarHidden(true, animated: false)
         
-        let v = UIView()
-        v.backgroundColor = .red
-        v.frame = UIScreen.main.bounds
-        
-        let imageView = UIImageView(image: UIImage(named: "logo"))
-        v.addSubview(imageView)
-        imageView.center = view.center
-        
-        
+//        let v = UIView()
+//        v.backgroundColor = .red
+//        v.frame = UIScreen.main.bounds
+//
+//        let imageView = UIImageView(image: UIImage(named: "logo"))
+//        v.addSubview(imageView)
+//        imageView.center = view.center
 //        https://github.com/flutter/flutter/issues/37818
 //        flutter engine 暂时有bug， 不推荐设置
 //        splashScreenView = v
+        
+        // 直接将 launchScreen.storyboard 中的view 作为 splashView
+        loadDefaultSplashScreenView()
+
     }
     
     required init(coder aDecoder: NSCoder) {
