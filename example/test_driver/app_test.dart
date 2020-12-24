470cc56ea14517ffb6851c74f9d5f41e2ae619bb
gix <aoxianglele@icloud.com>
2020-12-24 18:05:53 +0800

    [WIP] waiting integration_test null safety version.

--- example/test_driver/app_test.dart
+++ example/test_driver/app_test.dart
@@ -1,25 +1,29 @@
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:g_faraday_example/main.dart' as app;
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:integration_test/integration_test.dart';
 
 void main(List<String> args) {
   group('route', () {
-    FlutterDriver? driver;
-
     setUpAll(() async {
-      driver = await FlutterDriver.connect();
-      await driver?.waitUntilNoTransientCallbacks();
+      IntegrationTestWidgetsFlutterBinding.ensureInitialized();
     });
 
-    tearDownAll(() async {
-      // driver?.close();
+    testWidgets('flutter to native', (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      final f2nFinder = find.byKey(ValueKey('flutter2native'));
+      await tester.tap(f2nFinder);
     });
 
-    test('flutter to native', () async {
-      final f2nFinder = find.byValueKey('flutter2native');
-      await driver?.tap(f2nFinder);
+    // test('flutter to native', () async {
+    //   final f2nFinder = find.byValueKey('flutter2native');
+    //   await driver?.tap(f2nFinder);
 
-      final pageFinder = find.byValueKey('flutter2native_page');
-      final image = await driver?.screenshot();
-    });
+    //   final pageFinder = find.byValueKey('flutter2native_page');
+    //   final image = await driver?.screenshot();
+    // });
   });
 }
