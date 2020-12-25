9e75243c16ee6a8f101ddc080176c39c0160cbdb
Edward <edward6934@dingtalk.com>
2020-12-25 14:57:26 +0800

    Revert "update: FaradayActivity#pageId"
    This reverts commit ea455eef

--- android/src/main/kotlin/com/yuxiaor/flutter/g_faraday/FaradayActivity.kt
+++ android/src/main/kotlin/com/yuxiaor/flutter/g_faraday/FaradayActivity.kt
@@ -62,7 +62,8 @@ open class FaradayActivity : XFlutterActivity(), ResultProvider {
 
     }
 
-    private val pageId by lazy { intent.getIntExtra(FaradayConstants.ID, 0) }
+    private val pageId: Int
+        get() = intent.getIntExtra(FaradayConstants.ID, 0)
 
     private var resultListener: ((requestCode: Int, resultCode: Int, data: Intent?) -> Unit)? = null
 
