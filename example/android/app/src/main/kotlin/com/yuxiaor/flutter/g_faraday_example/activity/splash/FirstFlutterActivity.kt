b6884b4b36b46d9d3e1897dd7a442ab480c34c82
wsl <shuiling.wu@yuxiaor.com>
2024-01-27 15:59:15 +0800

    <fix> splash screen.

--- example/android/app/src/main/kotlin/com/yuxiaor/flutter/g_faraday_example/activity/splash/FirstFlutterActivity.kt
+++ example/android/app/src/main/kotlin/com/yuxiaor/flutter/g_faraday_example/activity/splash/FirstFlutterActivity.kt
@@ -3,6 +3,7 @@ package com.yuxiaor.flutter.g_faraday_example.activity.splash
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.window.SplashScreenView
 import com.yuxiaor.flutter.g_faraday.Faraday
 import com.yuxiaor.flutter.g_faraday.FaradayActivity
 import com.yuxiaor.flutter.g_faraday.channels.postNotification
@@ -10,10 +11,11 @@ import com.yuxiaor.flutter.g_faraday.channels.registerNotification
 import com.yuxiaor.flutter.g_faraday.channels.unregisterNotification
 import com.yuxiaor.flutter.g_faraday_example.widget.NotificationDialog
 
+
 class FirstFlutterActivity : FaradayActivity() {
 
     companion object {
-        fun build(context: Context) = builder<FirstFlutterActivity>("home", null, false).build(context)
+        fun build(context: Context) = builder<FirstFlutterActivity>("home", null, true).build(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,13 @@ class FirstFlutterActivity : FaradayActivity() {
 
         Faraday.registerNotification("GlobalNotification") {
             showNotification()
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            splashScreen
+                .setOnExitAnimationListener { splashScreenView: SplashScreenView -> splashScreenView.remove() }
         }
     }
 
